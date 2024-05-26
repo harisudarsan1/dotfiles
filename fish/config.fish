9de5c36afc44fde505dd7f5621036ac69e0c0516
Harisudarsan <sudarshanhari561@gmail.com>
2024-05-26 14:27:43 +0530

    new config

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source ./zoxide.fish
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias icat='kitty +kitten icat'
+alias ff='nvim $(fzf)'
+export EDITOR='nvim'
+export VISUAL='nvim'
+
+alias v='fd --type f --hidden --exclude .git | fzf-tmux -p --reverse | xargs nvim'
+alias ff='fzf|xargs nvim'
+alias mk='minikube'
+alias kgpa='kubectl get pods -A'
+alias kc='kubectl'
+alias kap='kubectl apply -f'
+alias kl='kubectl logs'
+
