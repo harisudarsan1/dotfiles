5c6aaee8f02c6014f249984e29fd193eeddef2ca
Harisudarsan <sudarshanhari561@gmail.com>
2025-05-04 23:17:53 +0530

    add

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ ln -s ~/dotfiles/nvim ~/.config/nvim -f
 ln -s ~/dotfiles/fish ~/.config/fish -f
 ln -s ~/dotfiles/lazygit ~/.config/lazygit -f
 ln -s ~/dotfiles/kitty ~/.config/kitty -f
-sudo ln -s ~/dotfiles/edit-config.sh /usr/bin/edc
+sudo ln -s ~/dotfiles/edit-config.sh /usr/bin/edc -f
 
