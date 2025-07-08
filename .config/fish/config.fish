d9d878a19d7b684a0452c954e6506bd1e446ec8f
Harisudarsan <sudarshanhari561@gmail.com>
2025-07-08 04:58:25 +0000

    change
    Signed-off-by: Harisudarsan <sudarshanhari561@gmail.com>

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,14 @@ end
 fish_vi_key_bindings
 
 bind --mode insert --sets-mode default jk repaint
+alias gc="git clone"
+alias kc="kubectl"
+alias kgpa="kubectl get pods -A"
+alias kd="kubectl delete"
+alias kdp="kubectl delete pod/"
+alias kl="kubectl logs"
+alias ka="kubectl apply -f"
+alias ke="EDITOR=vim kubectl edit"
+alias kde="kubectl describe"
+
+
